9a9ad9d176376d64b3e9f29b76e43068347ee3c7
Orion Edwards <orion.edwards@gmail.com>
2014-09-04 21:26:07 +1200

    Initial Commit

---
+++ MTGLifeCounter2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTGLifeCounter2
+//
+//  Created by Orion Edwards on 4/09/14.
+//  Copyright (c) 2014 Orion Edwards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

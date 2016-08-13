7ec4f2d700dca0450517a60e6a9751ec05bb141b
Orion Edwards <orion.edwards@gmail.com>
2016-08-13 23:42:04 +1200

    Fix deprecation warnings

--- MTGLifeCounter2/Colors.swift
+++ MTGLifeCounter2/Colors.swift
@@ -17,21 +17,21 @@ enum MtgColor : Int {
     var displayName:String {
         get {
             switch self {
-            case white: return "White"
-            case blue: return "Blue"
-            case black: return "Black"
-            case red: return "Red"
-            case green: return "Green"
-            case whiteBlue: return "Azorius"
-            case blueBlack: return "Dimir"
-            case blackRed: return "Rakdos"
-            case redGreen: return "Gruul"
-            case greenWhite: return "Selesnya"
-            case whiteBlack: return "Orzhov"
-            case blueRed: return "Izzet"
-            case blackGreen: return "Golgari"
-            case redWhite: return "Boros"
-            case greenBlue: return "Simic"
+            case .white: return "White"
+            case .blue: return "Blue"
+            case .black: return "Black"
+            case .red: return "Red"
+            case .green: return "Green"
+            case .whiteBlue: return "Azorius"
+            case .blueBlack: return "Dimir"
+            case .blackRed: return "Rakdos"
+            case .redGreen: return "Gruul"
+            case .greenWhite: return "Selesnya"
+            case .whiteBlack: return "Orzhov"
+            case .blueRed: return "Izzet"
+            case .blackGreen: return "Golgari"
+            case .redWhite: return "Boros"
+            case .greenBlue: return "Simic"
             }
         }
     }
@@ -48,28 +48,28 @@ enum MtgColor : Int {
         switch primary {
         case true:
             switch self {
-            case white, whiteBlue, whiteBlack:
+            case .white, .whiteBlue, .whiteBlack:
                 return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
-            case blue, blueBlack, blueRed:
+            case .blue, .blueBlack, .blueRed:
                 return UIColor(red: 0.0, green: 0.22, blue: 0.72, alpha: 1)
-            case black, blackRed, blackGreen:
+            case .black, .blackRed, .blackGreen:
                 return UIColor(red: 0.12, green: 0.19, blue: 0.25, alpha: 1)
-            case red, redGreen, redWhite:
+            case .red, .redGreen, .redWhite:
                 return UIColor(red: 0.84, green: 0.04, blue: 0.07, alpha: 1)
-            case green, greenWhite, greenBlue:
+            case .green, .greenWhite, .greenBlue:
                 return UIColor(red: 0.15, green: 0.68, blue: 0.27, alpha: 1)
             }
         case false:
             switch self {
-            case white, greenWhite, redWhite:
+            case .white, .greenWhite, .redWhite:
                 return UIColor(red: 0.97, green: 0.92, blue: 0.9, alpha: 1)
-            case blue, whiteBlue, greenBlue:
+            case .blue, .whiteBlue, .greenBlue:
                 return UIColor(red: 0.2, green: 0.30, blue: 1.00, alpha: 1)
-            case black, blueBlack, whiteBlack:
+            case .black, .blueBlack, .whiteBlack:
                 return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
-            case red, blackRed, blueRed:
+            case .red, .blackRed, .blueRed:
                 return UIColor(red: 0.78, green: 0.14, blue: 0.04, alpha: 1)
-            case green, redGreen, blackGreen:
+            case .green, .redGreen, .blackGreen:
                 return UIColor(red: 0.19, green: 0.66, blue: 0.20, alpha: 1)
             }
         }
